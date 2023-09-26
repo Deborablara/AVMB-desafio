591760e07edd2bf2d2bbb1474123d9fce4e2ae48
Deborablara <debora.blara19@gmail.com>
2023-09-26 19:34:49 -0300

    change route files

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -2,9 +2,9 @@ import express, { Router } from 'express';
 import { getUserId, getUserRepositories } from '../controllers/usuario';
 
 
-const userRouter: Router = express.Router();
+const usuarioRouter: Router = express.Router();
 
-userRouter.get('/id', getUserId);
-userRouter.post('/repositorios', getUserRepositories);
+usuarioRouter.get('/id', getUserId);
+usuarioRouter.post('/repositorios', getUserRepositories);
 
-export default userRouter;
+export default usuarioRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 
 import "reflect-metadata";
 import express from 'express';
+import bodyParser from "body-parser";
+import envelopeRouter from "./routes/envelope";
+import usuarioRouter from "./routes/usuario";
 
 const app = express();
 const port = 8080;
+
+app.use(bodyParser.json());
+
+app.use('/api/envelopes', envelopeRouter);
+app.use('/api/usuario', usuarioRouter);
+
 
 app.listen(port, () => {
   console.log(`Servidor Express rodando na porta ${port}`);
