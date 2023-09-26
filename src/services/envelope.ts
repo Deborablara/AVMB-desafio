40fa2723dfaf1b89882e13ef79970f8cd9686cda
Deborablara <debora.blara19@gmail.com>
2023-09-26 19:10:46 -0300

    create usuario service

--- src/services/envelope.ts
+++ src/services/envelope.ts
@@ -1,16 +1,9 @@
 import axios, { AxiosResponse } from 'axios';
 import { APIData, APIResponse } from './types/types';
-
-const apiURL = 'https://plataforma.astenassinatura.com.br/api/';
-const token = '274O5Td6rsR5QypP2huHk2OOJfr1FyeQ79p1tt3JCiIoH93GbnkwxF6S60yFQoZwYCzUwZVb-Lk9KvOx1EDnvfIQZw686hf3vJDivb0AfaXOgxUbp8n8vx6mYP1+0CEQmCzH+qStiAw8ZoKtUkawRanFROFIMGxC';
+import { apiURL, formatData } from '../utils/api';
 
 
-const formatData = (data: APIData): APIData => {
-  return {
-    token: token,
-    params: data,
-  };
-};
+
 
 export const newEnvelope = async (data: APIData): Promise<APIResponse> => {
   try {
---
+++ src/services/usuario.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import { apiURL, formatData } from '../utils/api';
+
+
+export const getId = async (data: any) => {
+  try {
+    const url: string = `${apiURL}getIdentificador`;
+    const formattedData = formatData(data);
+
+    const response = await axios.post(url, formattedData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(JSON.stringify(response.data));
+    }
+  } catch (error) {
+    throw new Error(error.response.data.error);
+  }
+};
+
+export const getUserRepos = async (data: any) => {
+  try {
+    const url: string = `${apiURL}getRepositoriosDoUsuario`;
+    const formattedData = formatData(data);
+
+    const response = await axios.post(url, formattedData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(JSON.stringify(response.data));
+    }
+  } catch (error) {
+    throw new Error(error.response.data.error);
+  }
+};
