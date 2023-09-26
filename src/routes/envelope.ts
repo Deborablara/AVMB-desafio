1ac901e5e79b4f202cb201d707a911215b162406
Deborablara <debora.blara19@gmail.com>
2023-09-26 19:23:05 -0300

    create envelope routes

---
+++ src/routes/envelope.ts
@@ -0,0 +1,19 @@
+import express, { Router } from 'express';
+import {
+  addSignatarioToEnvelope,
+  createEnvelope,
+  forwardEnvelopeForSignature,
+  getEnvelope,
+  getEnvelopesByRepo
+} from '../controllers/envelope';
+
+
+const envelopeRouter: Router = express.Router();
+
+envelopeRouter.post('/new', createEnvelope);
+envelopeRouter.post('/view', getEnvelope);
+envelopeRouter.post('/by-repo', getEnvelopesByRepo);
+envelopeRouter.post('/signatario', addSignatarioToEnvelope);
+envelopeRouter.post('/forward-for-signature', forwardEnvelopeForSignature);
+
+export default envelopeRouter;
